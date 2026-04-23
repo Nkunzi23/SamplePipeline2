a0585e0f51c428252eca6a450fe9fe0fbb2f1da5
Nkunzi23 <122588174+Nkunzi23@users.noreply.github.com>
2026-04-23 16:18:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return jsonify({
-        "message": "Hello from FBC Pipeline!",
+        "message": "Hello from my Sample Pipeline!",
         "status": "running"
     })
 
